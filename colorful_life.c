0cb0887302dead1ab968febc80edf20e0c92b9cc
Antoni Wellisz <awwellisz@uchicago.edu>
2023-10-23 02:52:25 -0500

    Updated toric board

--- colorful_life.c
+++ colorful_life.c
@@ -8,6 +8,8 @@
 #include <sys/time.h>
 
 #define SIZE 36
+#define WIDTH 36
+#define HEIGHT 36
 #define HT_SIZE 2048
 
 #define RESET  "\x1B[0m"
@@ -30,22 +32,22 @@
 
 enum modes { RANDOM, MANUAL, BATCH, PRESET };
 
-int count_neighbors(int state[SIZE][SIZE], int i, int j);
-void display(int state[SIZE][SIZE]);
+int count_neighbors(int state[SIZE][SIZE], int color[SIZE][SIZE], int i, int j, int *average_color);
+void display(int state[SIZE][SIZE], int color[SIZE][SIZE]);
 void parse_fen(char *fen, int (*state)[SIZE]);
 uint64_t rand_U64();
-void announce_winner(int state[SIZE][SIZE]);
 
 int main(int argc, char *argv[]) {
     // Used for keeping track of how long it's taken to render a frame
     //  to keep the framerate consistent. Probably not ideal
     struct timeval stop, start;
     long long start_ms, stop_ms, timediff;
+    int framerate = 10; // Frames per second
+    int ms_per_frame = 1000 / framerate; 
 
     int nseed = 0;
     int n = SIZE;
-    int num_neighbors, setup_mode, is_tribal, 
-        cycle_detect_on, i, j, preset_selection, speed_selection;
+    int num_neighbors, setup_mode, cycle_detect_on, i, j, preset_selection;
 
     // For FEN string parsing
     int fen_buffer_size = SIZE * SIZE + SIZE;
@@ -55,6 +57,8 @@ int main(int argc, char *argv[]) {
     int state[SIZE][SIZE] = {0};
     // Next game state
     int next[SIZE][SIZE] = {0};
+    // Color of each cell
+    int color[SIZE][SIZE] = {0};
     // Used for storing hash keys for each cell
     uint64_t hash_keys[SIZE][SIZE];
     // Initialize hash_keys to random values
@@ -65,13 +69,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Hash table
-    // Literally just an array lmao 
     uint64_t hash_table[HT_SIZE];
-
-    printf("ENTER A NUMBER TO CHOOSE LIFE MODE:\n");
-    printf("    0: SINGLE-ORGANISM\n");
-    printf("    1: TWO-ORGANISM (COMPETITIVE TRIBES)\n");
-    scanf("%d", &is_tribal);
 
     printf("DO YOU WANT CYCLE DETECTION ENABLED?\n");
     printf("(Answer '0' e.g. if you explicitly want to watch cyclic pattern repeat)\n");
@@ -79,33 +77,12 @@ int main(int argc, char *argv[]) {
     printf("    1: YES\n");
     scanf("%d", &cycle_detect_on);
 
-    printf("CHOOSE ANIMATION SPEED:\n");
-    printf("    0: 1 FPS (VERY SLOW)\n");
-    printf("    1: 5 FPS (SLOW)\n");
-    printf("    2: 10 FPS (NORMAL)\n");
-    printf("    3: 20 FPS (FAST)\n");
-    printf("    4: 50 FPS (VERY FAST)\n");
-    scanf("%d", &speed_selection);
-
     printf("ENTER A NUMBER TO CHOOSE SETUP MODE:\n");
     printf("    0: RANDOM\n");
     printf("    1: MANUAL (SET INDIVIDUAL CELLS)\n");
     printf("    2: BATCH MANUAL (INPUT FEN STRING)\n");
     printf("    3: VIEW PRESET OPTIONS\n");
     scanf("%d", &setup_mode);
-
-    int framerate = 10; // Frames per second
-    switch(speed_selection) {
-        case 0: framerate = 1; break;
-        case 1: framerate = 5; break;
-        case 2: framerate = 10; break;
-        case 3: framerate = 20; break;
-        case 4: framerate = 50; break;
-        default: 
-            printf("Invalid input.\n"); 
-            exit(0);
-    }
-    int ms_per_frame = 1000 / framerate; 
 
     switch(setup_mode) {
         case RANDOM:
@@ -120,12 +97,8 @@ int main(int argc, char *argv[]) {
                     if (rn1 < 0.6) {
                         state[i][j] = 0;
                     } else { 
-                        if (is_tribal && (j > n/2)) {
-                            state[i][j] = KOHM;
-                        } else {
-                            state[i][j] = YANG;
-                        }
-                        
+                        state[i][j] = 1;
+                        color[i][j] = (double)rand() / RAND_MAX * 255;
                     }
                 }
             }
@@ -136,7 +109,7 @@ int main(int argc, char *argv[]) {
                 scanf("%d,%d", &i, &j);
                 if (i == 0 && j == 0) break;
                 state[i][j] = 1;
-                display(state);
+                display(state, color);
             }
             break;
         case BATCH:
@@ -155,8 +128,8 @@ int main(int argc, char *argv[]) {
             printf("ENTER A NUMBER TO CHOOSE A PRESET:\n");
             printf("    0: GLIDER\n");
             printf("    1: R PENTOMINO\n");
-            printf("    2: IMPOSTOR (sus)\n");
-            printf("    3: 2 SQUARES (cool symmetric patterns)\n");
+            printf("    2: IMPOSTOR\n");
+            printf("    3: 2 SQUARES\n");
             scanf("%d", &preset_selection);
 
             switch(preset_selection) {
@@ -171,7 +144,7 @@ int main(int argc, char *argv[]) {
         default: printf("An error occured.\n"); exit(0);
     }
 
-    display(state);
+    display(state, color);
 
     // For keeping track of how many generations have been stored
     int hash_index = 0;
@@ -180,7 +153,6 @@ int main(int argc, char *argv[]) {
 
     // Main game loop
     while (true) {
-        printf("\x1b[?25l"); // hide the cursor
         total_generations++;
         // Keep track of when this frame began
         gettimeofday(&start, NULL);
@@ -189,39 +161,23 @@ int main(int argc, char *argv[]) {
         memset(next, 0, sizeof(int) * SIZE * SIZE);
 
         uint64_t hashkey = 0;
+        int average_color = 0;
 
         // Loop over all cells
         for (i = 0; i < n; i++) {
             for (j = 0; j < n; j++) {
-
-                num_neighbors = count_neighbors(state, i, j);
-
-                // Living Yangs
-                switch (state[i][j]) {
-                    case YANG:
-                        hashkey ^= hash_keys[i][j];
-
-                        if (num_neighbors == 2 || num_neighbors == 3) {
-                            next[i][j] = YANG;
-                        } else if (num_neighbors == -3) {
-                            next[i][j] = KOHM;
-                        }
-                        break;
-                    case KOHM:
-                        hashkey ^= ~(hash_keys[i][j]);
-
-                        if (num_neighbors == -2 || num_neighbors == -3) {
-                            next[i][j] = KOHM;
-                        } else if (num_neighbors == 3) {
-                            next[i][j] = YANG;
-                        }
-                        break;
-                    default: 
-                        if (num_neighbors == 3) {
-                            next[i][j] = YANG;
-                        } else if (num_neighbors == -3) {
-                            next[i][j] = KOHM;
-                        }
+                average_color = 0;
+                num_neighbors = count_neighbors(state, color, i, j, &average_color);
+                if (state[i][j]) {
+                    hashkey ^= hash_keys[i][j];
+                    if (num_neighbors == 2 || num_neighbors == 3) {
+                        next[i][j] = 1;
+                    }
+                } else {
+                    if (num_neighbors == 3) { 
+                        next[i][j] = 1;
+                        color[i][j] = average_color;
+                    }
                 }
             }
         }
@@ -230,10 +186,8 @@ int main(int argc, char *argv[]) {
         if (cycle_detect_on) {
             for (i = 0; i < HT_SIZE; i++) {
                 if (hashkey == hash_table[i]) {
-                    printf("\x1b[?25h"); // show the cursor
                     printf("Cycle detected with a period of %d generations.\n", hash_index - i + 1);
                     printf("Total game length: %d\n", total_generations);
-                    if (is_tribal) announce_winner(state);
                     exit(0);
                 }
             }
@@ -262,23 +216,23 @@ int main(int argc, char *argv[]) {
         if (timediff < ms_per_frame) {
             usleep(1000 * (ms_per_frame - timediff));
         }
-        display(state);
+        display(state, color);
     }
 }
 
 // Print current game state to screen
-void display(int state[SIZE][SIZE]) {
-    system("clear");
+void display(int state[SIZE][SIZE], int color[SIZE][SIZE]) {
     int i, j;
     int n = SIZE;
-    printf("\n");
-    
+    char color_code[16]; // should never be longer than 14(?) chars
+    printf("\n\n\n\n\n\n\n\n");
     for (i = 0; i < SIZE; i++) {
         for (j = 0; j < n; j++) {
-            switch (state[i][j]) {
-                case YANG: printf(BLUE "\u2588\u2588"); break;
-                case KOHM: printf(RED "\u2588\u2588"); break;
-                default: printf("  "); break;
+            if (state[i][j]) {
+                snprintf(color_code, sizeof(color_code), "\x1b[38;5;%dm", color[i][j]);
+                printf("%s\u2588\u2588", color_code); 
+            } else {
+                printf("  ");
             }
         }
         printf(RESET "\n");
@@ -286,20 +240,37 @@ void display(int state[SIZE][SIZE]) {
 }
 
 // Count number of neighbors around a cell
-int count_neighbors(int state[SIZE][SIZE], int i, int j) {
+int count_neighbors(int state[SIZE][SIZE], int color[SIZE][SIZE], int i, int j, int *average_color) {
+    int n = SIZE;
     int num_neighbors = 0;
+    int total_color = 0;
 
-    for(int di = -1; di <= 1; di++) {
-        for(int dj = -1; dj <= 1; dj++) {
+    // // Modulo arithmetic for toroidal board
+    // num_neighbors = state[(i-1+n)%n][(j-1+n)%n] + state[(i+n)%n][(j-1+n)%n] + state[(i+1)%n][(j-1+n)%n] +
+    //                 state[(i-1+n)%n][(j+n)%n]                              + state[(i+1)%n][(j+n)%n] +
+    //                 state[(i-1+n)%n][(j+1)%n] + state[(i+n)%n][(j+1)%n] + state[(i+1)%n][(j+1)%n];
+
+    // int total_color = color[(i-1+n)%n][(j-1+n)%n] + color[(i+n)%n][(j-1+n)%n] + color[(i+1)%n][(j-1+n)%n] +
+    //                 color[(i-1+n)%n][(j+n)%n]                              + color[(i+1)%n][(j+n)%n] +
+    //                 color[(i-1+n)%n][(j+1)%n] + color[(i+n)%n][(j+1)%n] + color[(i+1)%n][(j+1)%n];
+
+    for(int dx = -1; dx <= 1; dx++) {
+        for(int dy = -1; dy <= 1; dy++) {
             // Skip the cell itself
-            if(di == 0 && dj == 0) continue; 
+            if(dx == 0 && dy == 0) continue; 
             // Handling cyclic boundary
-            int ni = (i + di + SIZE) % SIZE;
-            int nj = (j + dj + SIZE) % SIZE;
+            int ni = (i + dx + WIDTH) % WIDTH;
+            int nj = (j + dy + HEIGHT) % HEIGHT;
+
             num_neighbors += state[ni][nj];
+            total_color += color[ni][nj];
         }
     }
+
+    *average_color = total_color / 8;
+
     return num_neighbors;
+
 }
 
 // Generate a random 64-bit number, used for hashing
@@ -309,22 +280,6 @@ uint64_t rand_U64() {
     return int1 | (int2 << 32);
 }
 
-// Find which tribe has more living cells at the end of the game
-void announce_winner(int state[SIZE][SIZE]) {
-    int tribal_balance = 0;
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
-            tribal_balance += state[i][j];
-        }
-    }
-    if (tribal_balance == 0) {
-        printf("It was a draw!\n");
-    } else if (tribal_balance > 0) {
-        printf("Yangs win with %d more cells!\n", tribal_balance);
-    } else {
-        printf("Kohms win with %d more cells!\n", tribal_balance * -1);
-    }
-}
 
 // Takes a FEN (Forsyth-Edwards Notation, adapted to Life) string and 
 //  places it on the current game board.
